7fb233cfcad36171448064a2c2f6c37bccb16953
NoritsuguHoshizawa <83053432+NHoshizawa@users.noreply.github.com>
2023-05-11 12:07:58 +0900

    Add files via upload

---
+++ kiguu/kiguu2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+void picture(i1,q1):
+	int i1=0;
+	int c1=0;
+	int a1=0;
+	char kigou[]={"クローバー","スペード","ハート","ダイヤ"};
+	int poi[]={1,2,3,4,5,6,7,8,9,10,11,12,13};
+	char np[13] ={"A ","2 ","3 ","4 ","5 ","6 ","7 ","8 ","9 ","10","J ","Q ","K "};
+	a1 = np[i1];
+	c1 = kigou[q1];
+	str xy[] = {"",
+		"----------",
+		"|        |",
+		     a1     ,
+		"|        |",
+		     c1     ,
+		"|        |",
+		     a1     ,
+		"|        |",
+		"----------",
+		""
+		};
+    for(int i = 0; i < std::end(xy) - std::begin(xy); ++i) 
+		{
+        std::cout << xy[i] << "\n";
+    	};
+};
