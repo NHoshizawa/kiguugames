97c64dd92584490773819c21bd9b15dd7ce44355
NoritsuguHoshizawa星澤宜嗣 <83053432+NHoshizawa@users.noreply.github.com>
2024-05-13 15:11:31 +0900

    Add files via upload

--- kiguu/kakunin.py
+++ kiguu/kakunin.py
@@ -7,19 +7,20 @@ def kanri(file1):
 	with open(str(file1) + '.pickle', 'rb') as f:
 		hozon1 = pickle.load(f)
 		hozon1 = int(hozon1)
-	print("Watashiha,robo63toAnatanoMainasuPointWoZeroniShimasu.MainasuPointHa,Nakushitahougarakudesuyo!!Douitashimasuka?")
-	answer = input("input→y/n?:")
-	answer = str(answer)
-	if answer == "y":
-		print("大変申し訳ございませんでした。pointを０pointに変換します。手続き文言：くんくんくん...、わんっ!!ChannToOmiTouShiDaWan！？")
-		hozon1 = int(0)
-		with open(str(file1) +'.pickle', mode='wb') as f:
-			pickle.dump(hozon1, f)
-		print(str(hozon1) + "ポイント保存されております。")
-		end = input("終了は何かキーを押してエンターキーを押してください:")
-		end = str(end)
-		if end == end:
-			return 0
+	if hozon1 <0:
+		print("Watashiha,robo63toAnatanoMainasuPointWoZeroniShimasu.MainasuPointHa,Nakushitahougarakudesuyo!!Douitashimasuka?")
+		answer = input("input→y/n?:")
+		answer = str(answer)
+		if answer == "y":
+			print("大変申し訳ございませんでした。pointを０pointに変換します。手続き文言：くんくんくん...、わんっ!!ChannToOmiTouShiDaWan！？")
+			hozon1 = int(0)
+			with open(str(file1) +'.pickle', mode='wb') as f:
+				pickle.dump(hozon1, f)
+			print(str(hozon1) + "ポイント保存されております。")
+			end = input("終了は何かキーを押してエンターキーを押してください:")
+			end = str(end)
+			if end == end:
+				return 0
 	else:
 		print(str(hozon1) + "ポイント保存されております。")
 		end = input("終了は何かキーを押してエンターキーを押してください:")
