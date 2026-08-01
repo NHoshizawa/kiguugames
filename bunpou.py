4429c6c9ac8ad1912459ed419554526ee6a4ad0c
NoritsuguHoshizawa星澤宜嗣 <83053432+NHoshizawa@users.noreply.github.com>
2026-08-01 12:53:28 +0900

    Add files via upload

---
+++ bunpou.py
@@ -0,0 +1,30 @@
+aaa=["S主語","V動詞","C補語","O目的語"]
+aab=["名詞","形容詞","副詞"]
+aac=["語","句","節"]
+aad=["SV","SVC","SV O","SVO1O2","SVOC"]
+bunkeiburui=0
+hinshi=0
+kumikei=0
+bunkei=0
+a1=0
+a2=0
+a3=0
+a4=0
+def baa():
+	bunkeiburui=int(input('文型文類？0〜4:'))
+	hinshi=int(input('品詞?0〜2:'))
+	kumikei=int(input('語=0 句=1 節=2 いずれか？:'))
+	bunkei=int(input('文型1なら0,文型2なら1,文型3なら2,文型4なら3,文型5なら4 のうち0〜4?:'))
+	a1=str(aaa[bunkeiburui])
+	a2=str(aab[hinshi])
+	a3=str(aac[kumikei])
+	a4=str(aad[bunkei])
+	return a1,a2,a3,a4
+def main():
+	b1,b2,b3,b4= baa()
+	print(b1+b2+b3+b4)
+	return 0
+main()
+	
+	
+	
