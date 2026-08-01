4a2403228dcf1af17c85e3053215bffaf77d9b4a
NoritsuguHoshizawa星澤宜嗣 <83053432+NHoshizawa@users.noreply.github.com>
2026-08-01 14:08:49 +0900

    Update aab list and input prompts in bunpou.py

--- bunpou.py
+++ bunpou.py
@@ -1,5 +1,6 @@
 aaa=["S主語","V動詞","C補語","O目的語"]
-aab=["名詞","形容詞","副詞"]
+aab=["名詞的→同格や主語や目的語や補語","形容詞的→名詞を説明、主語や間接目的語=補語になる、","副詞的→動詞を修飾〜するために"
+]
 aac=["語","句","節"]
 aad=["SV","SVC","SV O","SVO1O2","SVOC"]
 bunkeiburui=0
@@ -12,7 +13,8 @@ a3=0
 a4=0
 def baa():
 	bunkeiburui=int(input('文型文類？0〜4:'))
-	hinshi=int(input('品詞?0〜2:'))
+	hinshi=int(input('語句節の品詞(用法
+)?0〜2:'))
 	kumikei=int(input('語=0 句=1 節=2 いずれか？:'))
 	bunkei=int(input('文型1なら0,文型2なら1,文型3なら2,文型4なら3,文型5なら4 のうち0〜4?:'))
 	a1=str(aaa[bunkeiburui])
